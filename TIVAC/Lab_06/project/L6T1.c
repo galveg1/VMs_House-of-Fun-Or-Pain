52cec5b3bddd4a7160f3dedd2474434316b32ca9
galveg1 <36391457+galveg1@users.noreply.github.com>
2018-10-14 22:55:43 -0700

    Lab 6
    Forgot to add seperate task files

---
+++ TIVAC/Lab_06/project/L6T1.c
@@ -0,0 +1,81 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/debug.h"
+#include "driverlib/pwm.h"
+#include "driverlib/pin_map.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/rom.h"
+
+//Constants for Min and Max
+//Values
+#define PWM_FREQUENCY 55
+
+
+int main(void)
+     {
+    volatile uint32_t ui32Load;
+    volatile uint32_t ui32PWMClock;
+    volatile uint8_t ui8Adjust;
+    ui8Adjust = 83;
+
+
+    //Setup clock 40MHz
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
+    ROM_SysCtlPWMClockSet(SYSCTL_PWMDIV_64);  //40MHz/64 = 625KHz
+
+    //PWM Configure, PORTF with Peripherals, PWM1
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);//PWM output at PD0
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);//Enable LPad buttons
+    //PF1 set as PWM Out
+    ROM_GPIOPinTypePWM(GPIO_PORTD_BASE, GPIO_PIN_0);
+    ROM_GPIOPinConfigure(GPIO_PD0_M1PWM0);
+
+
+
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
+    ROM_GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0, GPIO_DIR_MODE_IN);
+    ROM_GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    //Count loaded based PWM ƒ
+    ui32PWMClock = SysCtlClockGet() / 64;
+    ui32Load = (ui32PWMClock / PWM_FREQUENCY) - 1;
+    PWMGenConfigure(PWM1_BASE, PWM_GEN_0, PWM_GEN_MODE_DOWN);
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_0, ui32Load);
+
+    ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui8Adjust * ui32Load / 1000);//1.82µs * 83 ~1.5ms the neutral position
+    ROM_PWMOutputState(PWM1_BASE, PWM_OUT_0_BIT, true);
+    ROM_PWMGenEnable(PWM1_BASE, PWM_GEN_0);
+
+
+    while(1)
+    {
+        if(ROM_GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)==0x00)
+        {
+            ui8Adjust--;
+            if (ui8Adjust < 30)
+            {
+                ui8Adjust = 30;//1.82µs * 30 ~.55ms 0°
+            }
+            ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui8Adjust * ui32Load / 1000);
+        }//enf if1
+
+        if(ROM_GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_0)==0x00)
+        {
+            ui8Adjust++;
+            if (ui8Adjust > 125)
+            {
+                ui8Adjust = 125;//1.82µs * 125 ~2.28ms 180°
+            }
+            ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui8Adjust * ui32Load / 1000);
+        }//end if2
+        ROM_SysCtlDelay(13000);
+    }//end while(1)
+
+}//Main
